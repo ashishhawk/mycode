ff280cf5529315e284918a6f7643b3768e0e5394
ashishsrivastava <ashishsrivastava@localhost>
2020-09-02 14:12:11 +0530

    new code

---
+++ demomyspringboot/src/main/java/com/myapplication/demomyspringboot/DemomyspringbootApplication.java
@@ -0,0 +1,13 @@
+package com.myapplication.demomyspringboot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemomyspringbootApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DemomyspringbootApplication.class, args);
+	}
+
+}
